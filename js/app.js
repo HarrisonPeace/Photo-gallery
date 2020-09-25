d08da87d3aeceaca268bb725622b9b532b3feb41
Harrison Peace <harrison@apwines.com>
2020-09-25 11:50:53 +1000

    Implimented search filter

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@ window.addEventListener('load', function() {
     captions: true
 });
 });
+
+function searchFunction() {
+  // Declare variables
+  var input, filter, ul, a, dataCaption, i, li;
+  input = document.getElementById('search');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchCriteria");
+  a = ul.getElementsByTagName('a');
+  li = ul.getElementsByTagName('li')
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < a.length; i++) {
+    dataCaption = a[i].getAttribute('data-caption');
+    if (dataCaption.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
